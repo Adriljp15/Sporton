ca6af717f26d4f48d380e20b37885603eb83b4a1
Adril JP <adriljupentus@gmail.com>
2026-01-14 12:18:58 +0700

    second commit

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "../globals.css";
 import Header from "./components/layouts/header";
+import Footer from "./components/layouts/footer";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -24,8 +25,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${poppins.variable} antialiased`}>
-          <Header/>
+          <Header />
           {children}
+          <Footer />
         </body>
     </html>
   );
